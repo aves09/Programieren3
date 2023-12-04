93bb197fa5a0a8ee74b2a0b71a939b26c76c561c
aves.09 <aves.09@tumo.world>
2023-12-04 17:20:52 +0100

    füge express server hinzu

--- Session4/server.js
+++ Session4/server.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("du hast es geschaft dich zu lbamieren")
+
+app.use(express.static("../"));
+app.get("/game", function(req, res){
+    res.redirect("../Aubschlussproject.html")
 });
+
+app.get("/", function(req, res){
+    res.send("<h1>hello my</h1><p>no way</p>")
+});
+app.get("/name/:name", function(req, res){
+    let name = req.params.name;
+    res.send("<h1>Hello" + name + "</h1>");
+});
+
+app.get("/google/:name1", function(req, res){
+    let name1 = req.params.name1
+   
+    res.redirect('https://www.google.com/search?q='+name1)
+});
+
+app.get("/*", function(req, res){
+    let name1 = req.params.name1
+   
+    res.send("<h1>File not Found</h1>")
+});
+
+
+
 
 app.listen(3000, function(){
     console.log("lssd")
